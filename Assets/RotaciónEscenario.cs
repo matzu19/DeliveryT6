30541fce0150422d1c2dd755669da3215ffb3d19
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-03-02 09:18:58 -0500

    Menu hecho, No funcional

---
+++ Assets/RotaciónEscenario.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaciónEscenario : MonoBehaviour
+{
+    //este cósigo solo hace que rote el pbjeto al que se le asigne.
+    //Pensado para ponerse en la cámara del Main menu
+
+    public float VelRotacion;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+      void Update()
+    {
+        transform.Rotate(new Vector3(0,VelRotacion/100, 0));
+    }
+}
