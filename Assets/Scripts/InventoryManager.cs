5db77eb38c3d95817f3b657bbdb2a186c49b9301
Santiago Mosquera <santiago.mosquerag@upb.edu.co>
2021-03-02 09:55:48 -0500

    inventory beta
    ya funcional, solo necesita los parametros especificos de cada recolectable

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     private GameObject[] bag;
     [SerializeField] private SelectManager selector;
+    [SerializeField] private Image[] image;
+    [SerializeField] private Sprite[] resources;
     private bool bagInUse;
 
     private void Update()
@@ -14,9 +17,22 @@ public class InventoryManager : MonoBehaviour
         {
             bag = selector.TemporaryInventory();
             for(int i = 0; i < bag.Length; i++)
-            {if(bag[i].name == "Cubo")
+            {
+                if(bag[i].name == "Cube")
                 {
-
+                    image[i].sprite = resources[1]; 
+                }
+                else if (bag[i].name == "Sphere")
+                {
+                    image[i].sprite = resources[2];
+                }
+                else if (bag[i].name == "Triangle")
+                {
+                    image[i].sprite = resources[3];
+                }
+                else if (bag[i].name == "box")
+                {
+                    image[i].sprite = resources[4];
                 }
             }
         }
