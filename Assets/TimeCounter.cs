4c22463de560c9d9d20857d574696883c4422ed8
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-03-16 12:25:31 -0500

    charge scene and audio persistance

--- Assets/TimeCounter.cs
+++ Assets/TimeCounter.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeCounter : MonoBehaviour
 {
-    int tiempo;
-    void Start()
+    public float tiempo;
+    public string nameLevel;
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Start()
+    {
+        StartCoroutine(waitLevel(tiempo));
     }
 
-    // Update is called once per frame
-    void Update()
+/*
+    public void CargaNivel(string Namenivel) //envia a escena por nombre de la misma
     {
-        
+
+        SceneManager.LoadScene(Namenivel);
+    }*/
+
+    IEnumerator waitLevel(float n)
+    {
+        yield return new WaitForSeconds(n);
+        //CargaNivel(nameLevel);
+        SceneManager.LoadScene(nameLevel);
     }
 }
---
+++ Assets/dontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroyOnLoad : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
