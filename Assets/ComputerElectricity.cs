e3cdd9592332650d23b052d6aa8b893d80363f1a
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-03-25 21:49:22 -0500

    Funcionalidad computador y palancas
    La funcionalikdad primaria está lista y puesta en los objetos, pero aúin falta apra que sea la finla. Flata UI PC

---
+++ Assets/ComputerElectricity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerElectricity : MonoBehaviour , IAction
+{
+    bool electricity;
+    public SwitchLever swithcOne, swithcTwo, swithcThree;
+/*
+    [SerializeField] private GameObject PressE, canvasPuzzle, canvasGeneral, reticula, camara; //Santiago
+    [SerializeField] private UnityStandardAssets.Characters.FirstPerson.FirstPersonController mouseLocking;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        electricity = false;
+        
+    }
+    //Método que cambia la electricidad a encendida. Debe de ir a los scripts de las palancas y activar su variable electricidad
+  
+        
+
+    public void Activate()
+    {
+        electricity = !electricity;
+        if (electricity)
+        {
+            swithcOne.stream = true;
+            swithcTwo.stream = true;
+            swithcThree.stream = true;
+            Debug.Log("Pc Stream Activated");
+        }
+        else
+        {
+            swithcOne.stream = false;
+            swithcTwo.stream = false;
+            swithcThree.stream = false;
+            Debug.Log("Pc Stream deactivated");
+
+        }
+    }
+}
---
+++ Assets/SwitchLever.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLever : MonoBehaviour, IAction
+{
+    [SerializeField]
+    private int n;
+    public bool stream;
+
+
+   
+    public void Activate()
+    {
+        Debug.Log("el estado de la corriente es " + stream);
+    }
+}
