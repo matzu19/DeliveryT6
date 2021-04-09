4e499b70ad4e2bb7d7799d8247084d9c8b59505f
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-04-09 10:44:40 -0500

    Revert "Revert "sounds items and iteracts""
    This reverts commit 830d1cc23c64baf64601d5925d6abf0ea2cc688d.

--- Assets/SwitchLever.cs
+++ Assets/SwitchLever.cs
@@ -8,27 +8,19 @@ public class SwitchLever : MonoBehaviour, IAction
     private int n;
     public bool stream;
     public bool win;
-
+    [SerializeField]
+    AudioSource activateSound;
     
-    void condicion1()
+
+    void Start()
     {
-        //if (stream)
-            //activar animación
+        activateSound = gameObject.GetComponent<AudioSource>();
     }
-    void condicion2()
-    {
-
-    }
-    void condicion3()
-    {
-
-    }
-
-
     public void Activate()
     {
         if (stream)
         {
+            activateSound.Play();
             this.transform.Rotate(0f, 0f, -80f);
             win = true;
         }
