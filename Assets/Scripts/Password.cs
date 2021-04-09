59fcf97853c83f8f1a62bc5a849da4a258fdf87a
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-04-09 11:58:44 -0500

    password

---
+++ Assets/Scripts/Password.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Password : MonoBehaviour
+{
+    private string input;
+    [SerializeField]private GameObject canvasField;
+    private void Start()
+    {
+        input = "britishman";
+    }
+
+    public void ReadPassword(string s)
+    {
+        s=s.ToLower();
+        if (s == input)
+        {
+            canvasField.SetActive(false);
+            Debug.Log("contraseña correcta");
+        }
+        else
+        {
+            Debug.Log("contraseña incorrecta");
+        }
+
+
+        
+    }
+}
