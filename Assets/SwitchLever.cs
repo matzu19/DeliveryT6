6496b9d4499c8a0365529eca521a25536dd5252f
Metz001 <46971952+Metz001@users.noreply.github.com>
2021-04-13 09:56:10 -0500

    Sound feedback damege lever improve

--- Assets/SwitchLever.cs
+++ Assets/SwitchLever.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchLever : MonoBehaviour, IAction
 {
@@ -9,13 +10,17 @@ public class SwitchLever : MonoBehaviour, IAction
     public bool stream;
     public bool win;
     [SerializeField] private Gloves guantes;
-    [SerializeField] GameObject canvaDamage;
+    [SerializeField] GameObject canvaDamage,g_message;
+    public Text message;
 
     AudioSource sound;
-    [SerializeField] AudioClip leverActivate, problem;
+    [SerializeField] AudioClip leverActivate;
+    [SerializeField] AudioClip[] problem = new AudioClip[3];
     
     void Start()
     {
+        g_message = message.gameObject;
+        g_message.SetActive(false);
         sound = gameObject.GetComponent<AudioSource>();
         canvaDamage.gameObject.SetActive(false);
     }
@@ -37,30 +42,45 @@ public class SwitchLever : MonoBehaviour, IAction
 
     public void Activate()
     {
-        Debug.Log("--Activate lever ");
-        if (stream /*&& guantes.guantesPuesto()*/)
+        //Debug.Log("--Activate lever ");
+        if (stream && guantes.guantesPuesto())
         {
             sound.clip = leverActivate;
             sound.Play();
             this.transform.Rotate(0f, 0f, -80f);
             win = true;
-
+            message.text = ("Lever is active");
+            g_message.SetActive(true);
+            StartCoroutine(waitTimeOn(g_message));
+        }
+        else if(stream)
+        {
+            canvaDamage.SetActive(true);
+            ProblemPlay();
+            sound.Play();
+            StartCoroutine(waitTimeOn(canvaDamage));
+            message.text = ("this stream could kill");
+            g_message.SetActive(true);
+            StartCoroutine(waitTimeOn(g_message));
         }
         else
         {
-            canvaDamage.SetActive(true);
-            sound.clip = problem;
-            sound.Play();
-            StartCoroutine(waitTimeOn());
-            
+            message.text = ("the machine has no energy");
+            g_message.SetActive(true);
+            StartCoroutine(waitTimeOn(g_message));
         }
         
         Debug.Log("el estado de la corriente es " + stream);
+
     }
 
-    IEnumerator waitTimeOn()
+    IEnumerator waitTimeOn(GameObject g)
     {
-        yield return new WaitForSeconds(1.3f);
-        canvaDamage.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        g.SetActive(false);
+    }
+    void ProblemPlay()
+    {
+        sound.clip = problem[Random.Range(0,problem.Length)];
     }
 }
