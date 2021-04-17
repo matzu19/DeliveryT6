5763dec6aaf5f276fdea22297446f8fdd1840af0
Santiago Mosquera <santiago.mosquerag@upb.edu.co>
2021-04-17 13:02:04 -0500

    .

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    private bool menuactivo;
+    [SerializeField] private UnityStandardAssets.Characters.FirstPerson.FirstPersonController mouseLocking;
+    private void Awake()
+    {
+        menuactivo = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && menuactivo == false)
+        {
+            Debug.Log("pauso");
+            menuactivo = true;
+            panel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && menuactivo == true)
+        {
+            Debug.Log("jugue");
+            menuactivo = false;
+            panel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    public void Continue()
+    {
+        Debug.Log("jugue");
+        menuactivo = false;
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
