dba9a306d04df0e60e0c0b899c6a9020cd07eb9f
matzu19 <mateo.zuluagag@upb.edu.co>
2021-05-08 11:33:49 -0500

    Mateo
    LastPuzzle

--- Assets/TouchRotateA.cs
+++ Assets/TouchRotateA.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class TouchRotateA : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] AudioSource flipSound;
     private void OnMouseDown()
     {
         if (!GameControlA.youWin)
         {
             transform.Rotate(0f, 0f, 90f);
-            
+            flipSound.Play();
         }
 
     }
