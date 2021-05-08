ffb70837681a663659a3eb53a9b57851de5995b2
matzu19 <mateo.zuluagag@upb.edu.co>
2021-05-08 11:12:19 -0500

    MateoLastPuzzle

---
+++ Assets/GameControlA.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlA : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] pictures;
+
+   
+    
+    public static bool youWin;
+
+    void Awake()
+    {
+        
+        //winText.SetActive(false);
+        youWin = false;
+    }
+
+    void Update()
+    {
+        if ((pictures[0].rotation.z == 0 || pictures[0].rotation.z == 360) && (pictures[1].rotation.z == 0 || pictures[1].rotation.z == 360) && (pictures[2].rotation.z == 0 || pictures[2].rotation.z == 360) && (pictures[3].rotation.z == 0 || pictures[3].rotation.z == 360))
+        {
+            youWin = true;
+            Debug.Log("ganast");
+           
+           
+        }
+
+    }
+    
+}
---
+++ Assets/TouchRotateA.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotateA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        if (!GameControlA.youWin)
+        {
+            transform.Rotate(0f, 0f, 90f);
+            
+        }
+
+    }
+}
