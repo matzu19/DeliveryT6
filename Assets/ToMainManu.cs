9ec0c8554d4a1546cc6540cc9853910bf793c05f
Metz001 <Demetz001@hotmail.com>
2021-05-21 23:14:39 -0500

    Game Final

--- Assets/ToMainManu.cs
+++ Assets/ToMainManu.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ToMainManu : MonoBehaviour
 {
+    [SerializeField]
+    GameObject text;
     // Start is called before the first frame update
     private void OnEnable()
     {
+        StartCoroutine("wait");
+       
+    }
+    
+    IEnumerator wait()
+    {
+        text.SetActive(true);
+        yield return new WaitForSeconds(2f);
         Application.Quit();
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
