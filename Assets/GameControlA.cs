360af13c1b21994c7a89824ba4c95001b569ed36
asdius <41333988+asdius@users.noreply.github.com>
2021-05-21 20:33:11 -0500

    asdius puerta

--- Assets/GameControlA.cs
+++ Assets/GameControlA.cs
@@ -7,22 +7,36 @@ public class GameControlA : MonoBehaviour
     [SerializeField]
     private Transform[] pictures;
 
+
     //[SerializeField] private GameObject camara, canvasGeneral, reticula, canvasPuzzle, puzzle1, boton //cambio de Santiago
     //[SerializeField] private UnityStandardAssets.Characters.FirstPerson.FirstPersonController mouseLocking;
     public static bool youWin;
     [SerializeField] private GameObject door;
+    bool gano;
 
     void Awake()
     {
+        gano = false;
         youWin = false;
     }
     void Update()
     {
         if ((pictures[0].rotation.z == 0 || pictures[0].rotation.z == 360) && (pictures[1].rotation.z == 0 || pictures[1].rotation.z == 360) && (pictures[2].rotation.z == 0 || pictures[2].rotation.z == 360) && (pictures[3].rotation.z == 0 || pictures[3].rotation.z == 360))
         {
+            print("se debe abrir puerta");
 
+            
             youWin = true;
-            Destroy(door);
+            gano = false;
+            //door.GetComponent<Animation>().Play();
+            door.GetComponent<Animator>().enabled=true;
+            //Destroy(door);
+            if (gano == false)
+            {
+                GetComponent<AudioSource>().Play();
+                gano = true;
+                print("debe reproducir sonido puerta");
+            }
         }
     }
     public bool IsWin()
